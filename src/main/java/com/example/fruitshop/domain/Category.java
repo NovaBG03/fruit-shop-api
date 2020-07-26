3dfd89c6220fc07ecafacd6be23dba782e789f84
Nikita Koynov <nikicha03@gmail.com>
2020-07-26 14:42:33 +0300

    project structure

---
+++ src/main/java/com/example/fruitshop/domain/Category.java
@@ -0,0 +1,13 @@
+package com.example.fruitshop.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Category {
+    private Long id;
+    private String name;
+}
